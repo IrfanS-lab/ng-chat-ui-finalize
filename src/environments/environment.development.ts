c3e3ae16cd4c46e5fed75d97939792c3979b1d86
IrfanS-lab <irfanshafie24@gmail>
2026-01-01 00:57:39 +0800

    inital commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://sqwvjwgjmyvddfgsvdhu.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxd3Zqd2dqbXl2ZGRmZ3N2ZGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMTYwNDQsImV4cCI6MjA4MjU5MjA0NH0.NEKrBosrxQHOfDq--o_ATR05agzdO5s6ug5skbbyFKs',
+};
